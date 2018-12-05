89ab91b6b20e7fb9802e2ef95ab12a4518d75878
早津渉 <hayatsuwataru@hayatsuwatarunoMacBook-puro.local>
2018-12-05 20:47:01 +0900

    とりあえずstoreまで作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "easy-peasy";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const model = {
+	todos: {
+		items: [],
+		addTodo: (state, payload) => {
+			state.items.push(payload);
+		}
+	}
+};
+const store = createStore(model);
+console.log(store.getState().todos.items);
+store.dispatch.todos.addTodo("Install easy-peasy");
+console.log(store.getState().todos.items);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
